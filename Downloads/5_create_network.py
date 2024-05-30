9a481551a93f80c21fc8d2bb3464f4f1e84e9c00
Kevin Wirtz <kevin-w@hotmail.fr>
2024-05-30 14:45:52 +0200

    New OA version

--- Downloads/5_create_network.py
+++ Downloads/5_create_network.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 
 client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client['UBI']
-collection = db['works_UBI']
+collection = db['works_UBI_20240517']
 
 #%% National Output
 
